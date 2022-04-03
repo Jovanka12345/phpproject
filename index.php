32242f983e03fd9406fd730b92affd49894e417e
Jovanka <jr20180105@student.fon.bg.ac.rs>
2022-04-03 17:48:39 +0200

    Final commit.

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    include 'header.php';
+?>
+
+<div class='container mt-2'>
+
+    <div class='row mt-5'>
+        <div class='col-4 mt-5'>
+            &nbsp;
+        </div>
+    <div class='col-4'>
+        <h1 align='center'>
+            WELCOME
+        </h1>
+    </div>
+    </div>
+        
+</div>
+
+
+<?php
+    include 'footer.php';
+?>
